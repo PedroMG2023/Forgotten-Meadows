6465f4fb647f88863cd689e3fe80820145e7fddf
Juan Rojas <juanrojas19304@gmail.com>
2024-06-27 16:35:24 -0600

    Fix link errors

--- includes/model_loading/mesh_load.h
+++ includes/model_loading/mesh_load.h
@@ -6,7 +6,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include <shader.h>
+#include <model_loading/shader.h>
 
 #include <string>
 #include <vector>
--- includes/model_loading/model.h
+++ includes/model_loading/model.h
@@ -5,13 +5,13 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <stb/stb_image.h>
+#include <stb_image.h>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-#include <mesh_load.h>
-#include <shader.h>
+#include <model_loading/mesh_load.h>
+#include <model_loading/shader.h>
 
 #include <string>
 #include <fstream>
