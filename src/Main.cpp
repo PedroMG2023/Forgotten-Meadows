3c5bbb7acd2c57b184ae6a65b8b7631b3c1e436a
Juan Rojas <juanrojas19304@gmail.com>
2024-06-28 17:01:04 -0600

    remove and change functions

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,13 +12,14 @@
 
 #include <SFML/Audio.hpp>
 #include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow* window);
+// callback functions
+void processInput(sf::Window& window, const Model& model);
+void scroll_callback(sf::Window& window, double xoffset, double yoffset);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -36,6 +37,7 @@ float lastFrame = 0.0f;
 
 int main()
 {
+    /*
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -144,61 +146,29 @@ int main()
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
+    */
     return 0;
 }
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
+void processInput(sf::Window& window, const Model& model)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        window.close();
 
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
         camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
         camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
         camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
-}
-
-// glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-{
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
-
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+void scroll_callback(sf::RenderWindow& window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
