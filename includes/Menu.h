350d1860b9cdf451c6063b2d62881b3aebb181c6
PedroMG2023 <devmgcoding03@gmail.com>
2024-07-03 18:28:29 -0600

    Add Menu class in Menu.h a class with methods and attribs necessary for the menu logic

--- includes/Menu.h
+++ includes/Menu.h
@@ -1,8 +1,52 @@
-//
-// Created by Miranda Developers on 3/7/2024.
-//
+#pragma once
+#include "SFML/Graphics.hpp"
+#define MAX_NUMBER_OF_ITEMS 3
 
-#ifndef MENU_H
-#define MENU_H
+enum MenuState { MAIN_MENU, OPTIONS_MENU };
 
-#endif //MENU_H
+#define MAX_NUMBER_OF_OPTIONS_ITEMS 3
+
+class Menu
+{
+public:
+    Menu(float width, float height);
+    ~Menu();
+
+    void draw(sf::RenderWindow& window);
+    void MoveUp();
+    void MoveDown();
+    bool loadResources();
+    void changeState(MenuState newState);
+    void toggle(int a);
+    bool isActive[2];
+
+
+    MenuState getCurrentState() { return currentState; }
+
+    //Get the window width and height
+    float getWindowWidth() { return rectangulo.getSize().x; }
+
+    int GetPressedItem() { return selectedItemIndex; }
+
+private:
+
+    MenuState currentState;
+    sf::Text optionsMenu[MAX_NUMBER_OF_OPTIONS_ITEMS];
+
+    int selectedItemIndex;
+
+    sf::ConvexShape selectorTriangle;
+    sf::RectangleShape rectangulo;
+    sf::Text Title;
+    sf::Font font;
+    sf::Font titleFont;
+    sf::Text menu[MAX_NUMBER_OF_ITEMS];
+    sf::Texture backgroundTexture;
+    sf::Sprite background;
+    sf::Vector2u textureSize;
+    sf::RectangleShape toggleShape[2];
+    sf::CircleShape circleShape[2];
+    float triangleY;
+    float scaleX;
+    float scaleY;
+};
